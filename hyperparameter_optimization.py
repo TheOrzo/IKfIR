32972d264415c69a982d94bbbe98126a0c6159ec
IsmailEnesCelik <57830794+Enes1097@users.noreply.github.com>
2024-08-16 21:17:41 +0200

    Tidied up the code

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -1,27 +1,32 @@
 import optuna
 import yaml
-import os
 import numpy as np
 import torch
 from torch import nn as nn
+
 from stable_baselines3 import PPO, DDPG, SAC
-from stable_baselines3.common.callbacks import EvalCallback, BaseCallback
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecNormalize
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3.common.utils import set_random_seed
-from robosuite.wrappers import GymWrapper
+
 import robosuite as suite
+from robosuite.wrappers import GymWrapper
 from robosuite.controllers import load_controller_config
-from optuna.visualization import plot_optimization_history, plot_slice
+
+from optuna.visualization import plot_optimization_history
 
 
-#optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
-#study_name = "PPO_Sawyer_OSC_POSE"  # Unique identifier of the study.
-study_name = "study1"
+# Set name of study
+study_name = "study_sawyer_pickplace"
 storage_name = "sqlite:///{}.db".format(study_name)
 
+# Load configuration
+with open("config_hyperparams.yaml") as stream:
+    config = yaml.safe_load(stream)
+
 # Define the environment setup
+# Make robosuite environment into a gym environment as stable baselines only supports gym environments
 def make_env(env_id, options, rank, seed=0):
     def _init():
         env = GymWrapper(suite.make(env_id, **options))
@@ -32,6 +37,7 @@ def make_env(env_id, options, rank, seed=0):
     set_random_seed(seed)
     return _init
 
+# Method to evaluate the policy
 def evaluate_policy(model, env, n_eval_episodes=5):
     all_episode_rewards = []
     for _ in range(n_eval_episodes):
@@ -45,10 +51,6 @@ def evaluate_policy(model, env, n_eval_episodes=5):
         all_episode_rewards.append(np.sum(episode_rewards))
     mean_reward = np.mean(all_episode_rewards)
     return mean_reward
-
-def save_model(model_path, model, vec_env):
-    model.save(model_path + ".zip")
-    vec_env.save(model_path + ".env")
 
 def objective(trial):
     # Suggest hyperparameters
@@ -85,54 +87,54 @@ def objective(trial):
         f"Value function coefficient: {vf_coef}, "
         f"Network architecture: {net_arch_type}")
 
-    # Load configuration
-    with open("config_hyperparams.yaml") as stream:
-        config = yaml.safe_load(stream)
+    # Set controller configuration
+    controller_config = load_controller_config(default_controller=config["controller"])
 
-    controller_config = load_controller_config(default_controller=config["robot_controller"])
-
+    # Setup environment
+    # Define environment parameters for specific environment "PickPlace"
     env_options = {
         "robots": config["robot_name"],
         "controller_configs": controller_config,
+        "gripper_types": config["gripper"],
         "has_renderer": False,
         "has_offscreen_renderer": True,
         "single_object_mode": 2,
         "object_type": "milk",
-        "use_camera_obs": True,
-        "use_object_obs": False,
-        "camera_names": "agentview",
-        "camera_heights": 128,
-        "camera_widths": 128,
-        "reward_shaping": True,
+        "use_camera_obs": True,         # provide image observations to agent
+        "use_object_obs": False,        # don't provide object observations to agent
+        "camera_names": "agentview",    # use "agentview" camera for observations
+        "camera_heights": 128,          # image height
+        "camera_widths": 128,           # image width
+        "reward_shaping": True,         # use a dense reward signal for learning
         "horizon": horizon,
         "control_freq": control_freq,
     }    
     
     # Setup environment
-    if config["multiprocessing"]:
-        env = SubprocVecEnv([make_env("PickPlace", env_options, i, config["seed"]) for i in range(config["num_envs"])], start_method='spawn')
-        eval_env = SubprocVecEnv([make_env("PickPlace", env_options, i, config["seed"]) for i in range(config["num_eval_envs"])], start_method='spawn')
-        eval_env = VecNormalize(eval_env)
-        # TODO: account when using multiple envs
-        if batch_size > n_steps:
-            batch_size = n_steps
+    env = SubprocVecEnv([make_env("PickPlace", env_options, i, config["seed"]) for i in range(config["num_envs"])], start_method='spawn')
+    env = VecNormalize(env)
+
+    # TODO: account when using multiple envs
+    if batch_size > n_steps:
+        batch_size = n_steps
+
+    # Check if cuda(linux) or mps(mac) is available
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print("Cuda backend is available.")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+        print("Mps backend is available.")
     else:
-        env = DummyVecEnv(make_env("PickPlace", env_options, 0, config["seed"]))
-        eval_env = DummyVecEnv([make_env("PickPlace", env_options, 0, config["seed"])])
-        eval_env = VecNormalize(eval_env)
-
-    if config["normalize"]:
-        env = VecNormalize(env)
-
-    device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+        device = torch.device("cpu")
+        print("Cuda backend is not available, using CPU.")
 
     # Orthogonal initialization
     ortho_init = False
 
     activation_fn_name = trial.suggest_categorical("activation_fn", ["tanh", "relu"])
 
-    # Independent networks usually work best
-    # when not working with images
+    # Independent networks usually work best when not working with images
     net_arch = {
         "tiny": dict(pi=[64], vf=[64]),
         "small": dict(pi=[64, 64], vf=[64, 64]),
@@ -170,22 +172,20 @@ def objective(trial):
     elif config["algorithm"] == "SAC":
         model = SAC(config["policy"], env, learning_rate=learning_rate, batch_size=batch_size, gamma=gamma, verbose=0, tensorboard_log=None, device=device)
     
-    '''
-    # Evaluation callback
-    eval_callback = EvalCallback(eval_env, best_model_save_path="./logs/",
-                                 log_path="./logs/", eval_freq=2048,
-                                 deterministic=True, render=False)
-    '''
-
     # Train the model
     model.learn(total_timesteps=total_timesteps, progress_bar=True)
-    
+    env.close()
+
+    # Setup evaluation environment
+    # Define environment parameters for specific environment "PickPlace"
+    eval_env = SubprocVecEnv([make_env("PickPlace", env_options, i, config["seed"]) for i in range(config["num_eval_envs"])], start_method='spawn')
+    eval_env = VecNormalize(eval_env)
+
     # Evaluate the model
-    #mean_reward = eval_callback.last_mean_reward
-    #mean_reward, _ = model.evaluate_policy(eval_env, n_eval_episodes=5, deterministic=True)
-    mean_reward = evaluate_policy(model, eval_env, n_eval_episodes=5)
+    mean_reward = evaluate_policy(model, eval_env, config["n_eval_episodes"])
     print("Mean reward: ", mean_reward)
-    
+    eval_env.close()
+
     trial.report(mean_reward, step=total_timesteps)
 
     #Handle pruning based on the intermediate value
@@ -196,8 +196,8 @@ def objective(trial):
 
 # Optimize hyperparameters
 if __name__ == "__main__":
-    study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=50, timeout=90000)
+    study = optuna.create_study(direction='maximize', study_name=study_name, storage=storage_name, load_if_exists=True)
+    study.optimize(objective, config["n_trials"])
 
     pruned_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.PRUNED]
     complete_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.COMPLETE]
